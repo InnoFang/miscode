3d8e19c3be38023c9a14dd5e6bacb7d9ca58227b
InnoFang <innofang@outlook.com>
2018-04-20 21:00:51 +0800

    :zap: update

---
+++ C Cpp/lispy/lispy.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <editline/readline.h>
+#include <editline/history.h>
+
+/* Declare a buffer for user input of size 2048 */
+static char input[2048];
+
+int main(int argc, char** argv) {
+	
+	/* Print Version and Exit Information */
+	puts("Lispy Version 0.0.0.0.1");
+	puts("Press Ctrl+C to Exit\n");
+	
+	/* In a never ending loop */
+	while (1) {
+		/* Output our prompt */
+		fputs("lispy> ", stdout);
+		
+		/* Read a line of user input of maximum size 2048 */
+		fgets(input, 2048, stdin);
+		
+		/* Echo input back to user */
+		printf("No you're a %s", input);
+	}
+	
+	return 0;
+}
