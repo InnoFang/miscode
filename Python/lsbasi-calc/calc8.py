743d7851ffcece90c6b81e8e7981d91118479658
InnoFang <innofang@outlook.com>
2018-05-11 12:52:55 +0800

    :sparkles: :art: update

--- Python/lsbasi-calc/calc8.py
+++ Python/lsbasi-calc/calc8.py
@@ -102,6 +102,12 @@ class Num(AST):
         self.value = token.value
 
 
+class UnaryOp(AST):
+    def __init__(self, op, expr):
+        self.token = self.op = op
+        self.expr = expr
+
+
 class Parser(object):
     def __init__(self, lexer):
         self.lexer = lexer
@@ -126,10 +132,18 @@ class Parser(object):
 
     def factor(self):
         """
-        factor : INTEGER | LPAREN expr RPAREN
+        factor : (PLUS | MINUS) factor | INTEGER | LPAREN expr RPAREN
         """
         token = self.current_token
-        if token.type == INTEGER:
+        if token.type == PLUS:
+            self.eat(PLUS)
+            node = UnaryOp(token, self.factor())
+            return node
+        elif token.type == MINUS:
+            self.eat(MINUS)
+            node = UnaryOp(token, self.factor())
+            return node
+        elif token.type == INTEGER:
             self.eat(INTEGER)
             return Num(token)
         elif token.type == LPAREN:
@@ -157,7 +171,7 @@ class Parser(object):
         
         expr   : factor ((PLUS | MINUS) factor) *
         term   : factor ((MUL | DIV) factor) * 
-        factor : INTEGER | LPAREN expr RPAREN
+        factor : (PLUS | MINUS) factor | INTEGER | LPAREN expr RPAREN
         """
         node = self.term()
 
@@ -201,6 +215,13 @@ class Interpreter(NodeVisitor):
 
     def visit_Num(self, node):
         return node.value
+
+    def visit_UnaryOp(self, node):
+        op = node.op.type
+        if op == PLUS:
+            return +self.visit(node.expr)
+        elif op == MINUS:
+            return -self.visit(node.expr)
 
     def interpret(self):
         tree = self.parser.parse()
