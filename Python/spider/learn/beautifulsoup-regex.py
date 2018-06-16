800557b68b05823eb4227a4c9f43c633eafbebac
InnoFang <innofang@outlook.com>
2018-06-16 23:05:18 +0800

    :sparkles: update python web spider learning code

---
+++ Python/spider/learn/beautifulsoup-regex.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import re
+
+html = urlopen('https://morvanzhou.github.io/static/scraping/table.html').read().decode('utf-8')
+# print(html)
+
+soup = BeautifulSoup(html, features='lxml')
+img_links = soup.find_all('img', {'src': re.compile('.*?\.jpg')})
+for link in img_links:
+    print(link['src'])
+
+course_links = soup.find_all('a', {'href': re.compile('https://morvan.*')})
+for link in course_links:
+    print(link['href'])
