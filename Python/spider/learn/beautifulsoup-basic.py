1f7a62db3203bdb762d3802e23af1b6561207367
InnoFang <innofang@outlook.com>
2018-06-16 20:12:32 +0800

    :sparkles: update

---
+++ Python/spider/learn/beautifulsoup-basic.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+html = urlopen("https://morvanzhou.github.io/static/scraping/basic-structure.html").read().decode('utf-8')
+# print(html)
+
+soup = BeautifulSoup(html, features='lxml')
+# print(soup.h1)
+# print('\n', soup.p)
+
+all_href = soup.find_all('a')
+all_href = [l['href'] for l in all_href]
+print('\n', all_href)
