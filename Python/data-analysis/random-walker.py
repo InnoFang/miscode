ea4c74f920cd7fb2920f9c42b0fa60c74a26c898
InnoFang <innofang@outlook.com>
2018-06-18 23:03:34 +0800

    :sparkles: update data-analysis learning code

--- Python/data-analysis/random-walker.py
+++ Python/data-analysis/random-walker.py
@@ -2,20 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random 
 
-position = 0
-walk = [position]
-steps = 1000
-for i in range(steps):
-    step = 1 if random.randint(0, 1) else -1
-    position += step
-    walk.append(position)
+# # normal method 
+# position = 0
+# walk = [position]
+# steps = 1000
+# for i in range(steps):
+#     step = 1 if random.randint(0, 1) else -1
+#     position += step
+#     walk.append(position)
+# x = np.linspace(0, 1000, 1001)
+# plt.figure()
+# plt.ylim((-15, 15))
+# plt.xlim((0, 100))
+# plt.title('Random walk with +1/-1 steps')
+# plt.plot(x, walk)
+# plt.show()
 
-x = np.linspace(0, 1000, 1001)
+# Use NumPy
+nsteps = 1000
+draws = np.random.randint(0, 2, size=nsteps)
+steps = np.where(draws > 0, 1, -1)
+walk = steps.cumsum()
 
-plt.figure()
-plt.ylim((-15, 15))
+print('the minium of steps : ', walk.min())
+print('the maxinum of steps : ', walk.max())
+
+x = np.linspace(0, nsteps, nsteps)
 plt.xlim((0, 100))
+plt.ylim((-15, 15))
 plt.title('Random walk with +1/-1 steps')
-
 plt.plot(x, walk)
 plt.show()
