845d7423f575b391b6061fe34446b95e7ab9e40d
InnoFang <innofang@outlook.com>
2018-09-13 13:18:46 +0800

    :art: update

--- Python/django-project/mysite/mysite/urls.py
+++ Python/django-project/mysite/mysite/urls.py
@@ -14,8 +14,33 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')), 
+    path('admin/', admin.site.urls), 
 ]
+
+'''
+函数 include() 允许引用其它 URLconfs。每当 Django 遇到 :func：~django.urls.include 时，它会截断与此项匹配的 URL 的部分，并将剩余的字符串发送到 URLconf 以供进一步处理。
+
+函数 path() 具有四个参数，两个必须参数：route 和 view，两个可选参数：kwargs 和 name
+
+ + route:
+
+    route 是一个匹配 URL 的准则（类似正则表达式）。当 Django 响应一个请求时，它会从 urlpatterns 的第一项开始，按顺序依次匹配列表中的项，直到找到匹配的项。
+
+    这些准则不会匹配 GET 和 POST 参数或域名。例如，URLconf 在处理请求 https://www.example.com/myapp/ 时，它会尝试匹配 myapp/ 。处理请求 https://www.example.com/myapp/?page=3 时，也只会尝试匹配 myapp/。
+
++ view:
+
+    当 Django 找到了一个匹配的准则，就会调用这个特定的视图函数，并传入一个 HttpRequest 对象作为第一个参数，被“捕获”的参数以关键字参数的形式传入。稍后，我们会给出一个例子。
+
++ kwargs:
+
+   任意个关键字参数可以作为一个字典传递给目标视图函数。本教程中不会使用这一特性。
+
++ name:
+
+   为你的 URL 取名能使你在 Django 的任意地方唯一地引用它，尤其是在模板中。这个有用的特性允许你只改一个文件就能全局地修改某个 URL 模式。
+'''
