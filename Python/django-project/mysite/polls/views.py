a14fae035065e060f21e90f8bfdfd53fd418fa6c
InnoFang <innofang@outlook.com>
2018-09-16 13:20:36 +0800

    :construction: do the detail and HTTP404 page

--- Python/django-project/mysite/polls/views.py
+++ Python/django-project/mysite/polls/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.http import Http404
+
 from .models import Question
 
 # Create your views here.
@@ -24,7 +26,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
