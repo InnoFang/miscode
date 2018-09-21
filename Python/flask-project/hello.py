9d1d6c045277c03ddb2cf5a6635aad331a6a9afa
InnoFang <innofang@outlook.com>
2018-09-21 22:36:00 +0800

    :art: update

--- Python/flask-project/hello.py
+++ Python/flask-project/hello.py
@@ -9,7 +9,7 @@ def hello():
 students = []
 
 @app.route('/students', methods=['POST',])
-def add_student():
+def add_students():
     if not request.json:
         abort(400)
 
@@ -27,6 +27,25 @@ def add_student():
     }
     students.append(student)
     return 'success'
+
+
+@app.route('/students/', methods=['PUT'])
+def update_students():
+    if not request.json or not 'id' in request.json:
+        abort(400)
+    update_id = int(request.json['id'])
+    student = filter(lambda t: t['id'] == update_id, students)
+    if len(student) == 0:
+        abort(400)
+    if not request.json:
+        abort(403)
+
+    student[0]['name'] = request.json.get('name', student[0]['name'])
+    student[0]['age'] = request.json.get('age', student[0]['age'])
+    student[0]['grade'] = request.json.get('grade', student[0]['grade'])
+    student[0]['birthplace'] = request.json.get('birthplace', student[0]['birthplace'])
+
+    return jsonify(student[0])
 
 
 if __name__ == '__main__':
