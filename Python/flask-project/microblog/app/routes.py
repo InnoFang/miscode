38a21f240540c7ecef93d1a322fd36cc94411aa9
InnoFang <innofang@outlook.com>
2018-09-24 12:52:45 +0800

    :construction: receive form and add validation

--- Python/flask-project/microblog/app/routes.py
+++ Python/flask-project/microblog/app/routes.py
@@ -21,5 +21,8 @@ def index():
 @app.route('/login')
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
-    
