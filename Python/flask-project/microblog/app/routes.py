4f1e330a627837c589d61b5281637e405590927c
InnoFang <innofang@outlook.com>
2018-09-26 12:25:44 +0800

    :sparkles: user logout

--- Python/flask-project/microblog/app/routes.py
+++ Python/flask-project/microblog/app/routes.py
@@ -1,8 +1,9 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from app.models import User
+
 
 @app.route('/')
 @app.route('/index')
@@ -34,3 +35,9 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
