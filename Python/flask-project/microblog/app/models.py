a2df8a0796275e947c4a8a9aaa55facde953c39c
InnoFang <innofang@outlook.com>
2018-09-26 12:08:11 +0800

    :construction: hash password

--- Python/flask-project/microblog/app/models.py
+++ Python/flask-project/microblog/app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 class User(db.Model):
@@ -8,6 +9,12 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
