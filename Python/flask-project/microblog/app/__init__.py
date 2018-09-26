49a3f2ace87aeb4ccbab57acc039d23857df2477
InnoFang <innofang@outlook.com>
2018-09-26 12:34:28 +0800

    :sparkles: require user to login

--- Python/flask-project/microblog/app/__init__.py
+++ Python/flask-project/microblog/app/__init__.py
@@ -10,4 +10,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 
+# 'login'值是登录视图函数（endpoint）名，换句话说该名称可用于url_for()函数的参数并返回对应的URL
+login.login_view = 'login'
+
+
 from app import routes, models
--- Python/flask-project/microblog/app/routes.py
+++ Python/flask-project/microblog/app/routes.py
@@ -1,12 +1,14 @@
 from app import app
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Inno'}
     posts = [
@@ -33,7 +35,10 @@ def login():
             flash('Invalid username of password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 
