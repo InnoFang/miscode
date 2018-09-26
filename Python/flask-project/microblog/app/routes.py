1178f21fd364d24bd2a7a7b219199600cd3ef48b
InnoFang <innofang@outlook.com>
2018-09-26 12:39:00 +0800

    :art: update the indx template

--- Python/flask-project/microblog/app/routes.py
+++ Python/flask-project/microblog/app/routes.py
@@ -21,7 +21,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
