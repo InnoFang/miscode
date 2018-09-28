fcefbbc009aa229d3a3b3fb7a10961f9db02c4a6
InnoFang <innofang@outlook.com>
2018-09-28 11:23:25 +0800

    :art: update User model

--- Python/flask-project/microblog/app/models.py
+++ Python/flask-project/microblog/app/models.py
@@ -15,6 +15,8 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
