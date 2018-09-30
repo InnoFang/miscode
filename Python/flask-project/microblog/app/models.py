40a1c1d90dc7030566e2b8e117fa81d2b0e0cfce
InnoFang <innofang@outlook.com>
2018-09-30 22:58:03 +0800

    :sparkles: add followers table

--- Python/flask-project/microblog/app/models.py
+++ Python/flask-project/microblog/app/models.py
@@ -4,6 +4,10 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from hashlib import md5
 from flask_login import UserMixin
 
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
+
 
 @login.user_loader
 def load_user(id):
@@ -18,7 +22,12 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
