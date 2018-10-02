406fc8db973c270513e4f2c544cb821703283336
InnoFang <innofang@outlook.com>
2018-10-02 12:46:41 +0800

    :sparkles: pagination for users post

--- Python/flask-project/microblog/app/routes.py
+++ Python/flask-project/microblog/app/routes.py
@@ -73,11 +73,15 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'The post #1'},
-        {'author': user, 'body': 'The post #12'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.before_request
--- Python/flask-project/microblog/config.py
+++ Python/flask-project/microblog/config.py
@@ -17,3 +17,4 @@ class Config(object):
     ADMINS = ['your-email@example.com']
 
     POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 25
