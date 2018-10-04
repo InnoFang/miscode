6900773708cbd38df424af63f38a0babef43a299
InnoFang <innofang@outlook.com>
2018-10-04 12:34:23 +0800

    :sparkles: use flask-moment and moment.js to format the date and time

--- Python/flask-project/microblog/app/__init__.py
+++ Python/flask-project/microblog/app/__init__.py
@@ -8,6 +8,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,6 +17,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 # 'login'值是登录视图函数（endpoint）名，换句话说该名称可用于url_for()函数的参数并返回对应的URL
 login.login_view = 'login'
