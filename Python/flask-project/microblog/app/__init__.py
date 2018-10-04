0e94acba47b34e33be0ab5bb4963c09794504040
InnoFang <innofang@outlook.com>
2018-10-04 12:20:44 +0800

    :lipstick: use flask-bootstrap to beautify UI

--- Python/flask-project/microblog/app/__init__.py
+++ Python/flask-project/microblog/app/__init__.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,6 +15,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 # 'login'值是登录视图函数（endpoint）名，换句话说该名称可用于url_for()函数的参数并返回对应的URL
 login.login_view = 'login'
