83321b9b307c41bad973a37f39709cdf094ec3ff
InnoFang <innofang@outlook.com>
2020-05-04 19:39:19 +0800

    :art: udpate Scala code

---
+++ Scala/scala-train/src/main/scala/io/github/innofang/ArrayTest.scala
@@ -0,0 +1,39 @@
+package io.github.innofang
+
+import scala.collection.mutable.ArrayBuffer
+
+object ArrayTest extends App {
+
+  // 定长数组
+  val arr1 = new Array[Int](5)
+  for (i <- 1 until 5) {
+    arr1(i) = i
+  }
+  println(arr1.min)
+  println(arr1.max)
+  println(arr1.sum)
+  println(arr1.mkString(","))
+
+  // 可变长数组
+  var arr2 = ArrayBuffer[Int]()
+  // add data
+  arr2 += 1           // 加一个元素
+  arr2 += (2, 3, 4)   // 加一组元素
+  arr2 ++= Array(5, 6, 7) // 加一个数组的元素
+  arr2.insert(0, 0)   // 第 0 位置插入元素 0
+  arr2.remove(1)      // 删除下标为 1 的元素
+  arr2.remove(0, 3) // 删除 [0, 3) 位置的元素
+  arr2.trimEnd(2)   // 删除最后面两个元素
+
+  // 遍历
+  for (ele <- arr2) {
+    println(ele)
+  }
+  println()
+  // 逆序遍历
+  for (i <- (0 until arr2.length).reverse) {
+    println(arr2(i))
+  }
+
+  println(arr2)
+}
---
+++ Scala/scala-train/src/main/scala/io/github/innofang/ListTest.scala
@@ -0,0 +1,5 @@
+package io.github.innofang
+
+class ListTest {
+
+}
