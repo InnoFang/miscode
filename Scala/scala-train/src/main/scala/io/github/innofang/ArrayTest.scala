2e5ebd5559cda2a57dceee97ee204c9f15d38ad3
InnoFang <innofang@outlook.com>
2020-05-04 20:01:52 +0800

    :art: udpate Scala code

--- Scala/scala-train/src/main/scala/io/github/innofang/ArrayTest.scala
+++ Scala/scala-train/src/main/scala/io/github/innofang/ArrayTest.scala
@@ -18,12 +18,16 @@ object ArrayTest extends App {
   var arr2 = ArrayBuffer[Int]()
   // add data
   arr2 += 1           // 加一个元素
-  arr2 += (2, 3, 4)   // 加一组元素
-  arr2 ++= Array(5, 6, 7) // 加一个数组的元素
+  arr2 += 2           // 加一个元素
+  arr2 += (3, 4, 5, 6)   // 加一组元素
+  arr2 ++= Array(7, 8, 9, 10) // 加一个数组的元素
   arr2.insert(0, 0)   // 第 0 位置插入元素 0
   arr2.remove(1)      // 删除下标为 1 的元素
   arr2.remove(0, 3) // 删除 [0, 3) 位置的元素
   arr2.trimEnd(2)   // 删除最后面两个元素
+  arr2 -= 5   // remove 删除的是下标，-= 删除的是元素
+  arr2 -= (6, 7)
+  arr2 --= Array(9, 11) // 删除的元素不存在也不会出错
 
   // 遍历
   for (ele <- arr2) {
