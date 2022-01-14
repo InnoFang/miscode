b473fcdd1b35a43b3c38ced87c525643bdcda3c1
InnoFang <innofang@outlook.com>
2022-01-14 15:35:12 +0800

    :bug: fix

--- Rust/s1/tcpserver/src/main.rs
+++ Rust/s1/tcpserver/src/main.rs
@@ -6,9 +6,9 @@ fn main() {
 
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
-        println!("Connection established!")
-        let mut buffer = [0; 1024];
+        println!("Connection established!");
 
+        let mut buffer = [0; 1024];
         stream.read(&mut buffer).unwrap();
         stream.write(&mut buffer).unwrap();
     }
