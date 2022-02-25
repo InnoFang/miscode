d5988f9e5b880693f2a8ad16e1203c93504b563f
InnoFang <innofang@outlook.com>
2022-02-25 22:34:17 +0800

    :tada: init a snake game project

---
+++ Rust/mini-programs/snake/src/game.rs
@@ -0,0 +1,82 @@
+use piston_window::*;
+use piston_window::types::Color;
+
+use rand::{thread_rng, Rng};
+use crate::snake::{Direction, Snake};
+use crate::draw::{draw_block, draw_rectangle};
+
+const FOOD_COLOR: Color = [0.80, 0.00, 0.00, 1.0];
+const BORDER_COLOR: Color = [0.00, 0.00, 0.00, 1.0];
+const GAMEOVER_COLOR: Color = [0.90, 0.00, 0.00, 0.5];
+
+const MOVING_PERIOD: f64 = 0.1;
+const RESTART_TIME: f64 = 1.0;
+
+pub struct Game {
+    snake: Snake,
+
+    food_exists: bool,
+    food_x: i32,
+    food_y: i32,
+
+    width: i32,
+    height: i32,
+
+    game_over: bool,
+    waiting_time: f64,
+}
+
+impl Game {
+    pub fn new(width: i32, height: i32) -> Game {
+        Game { 
+            snake: Snake::new(2, 2),
+            waiting_time: 0.0, 
+            food_exists: true,
+            food_x: 6,
+            food_y: 4,
+            width,
+            height,
+            game_over: false
+        }
+    }
+
+    pub fn key_pressed(&mut self, key: Key) {
+        if self.game_over {
+            return;
+        }
+
+        let dir = match key {
+            Key::Up => Some(Direction::Up),
+            Key::Down = Some(Direction::Down),
+            Key::Left => Some(Direction::Left),
+            Key::Right => Some(Direction::Right),
+            _ => None
+        };
+        if dir.unwrap() == self.snake.head_direction().opposite() {
+            return;
+        }
+
+        self.update_snake(dir);
+    }
+
+    pub fn draw(&self, con: &Context, g: &mut G2d) {
+        self.snake.draw(con, g);
+
+        if self.food_exists {
+            draw_block(FOOD_COLOR, self.food_x, self.food_y, con, g);
+        }
+
+        draw_rectangle(BORDER_COLOR, 0, 0, self.width, 1, con, g);
+        draw_rectangle(BORDER_COLOR, 0, self.height - 1, self.width, 1, con, g);
+        draw_rectangle(BORDER_COLOR, 0, 0, 1, self.height, con, g);
+        draw_rectangle(BORDER_COLOR, self.width - 1, 0, 1, self.height, con, g);
+
+        if self.game_over {
+            draw_rectangle(GAMEOVER_COLOR, 0, 0, self.width, self.height, con, g);
+        }
+    }
+
+    pub fn update_snake(&self, dir: Direction) {
+
+    }
+}
---
+++ Rust/mini-programs/snake/src/main.rs
@@ -0,0 +1,10 @@
+extern crate rand;
+extern crate piston_window;
+
+mod draw;
+mod snake;
+mod game;
+
+fn main() {
+    println!("Hello, world!");
+}
