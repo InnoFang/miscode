8c8b3961e3c92c7a13bd96a93caac273b3fc51c8
InnoFang <innofang@outlook.com>
2022-02-25 22:48:28 +0800

    :art: update game.rs

--- Rust/mini-programs/snake/src/game.rs
+++ Rust/mini-programs/snake/src/game.rs
@@ -76,7 +76,74 @@ impl Game {
         }
     }
 
-    pub fn update_snake(&self, dir: Direction) {
+    pub fn update(&mut self, delta_time: f64) {
+        self.waiting_time -= delta_time;
 
+        if self.game_over {
+            if self.waiting_time > RESTART_TIME {
+                self.restart();
+            }
+            return;
+        }
+
+        if !self.food_exists {
+            self.add_food();
+        }
+
+        if self.waiting_time > MOVING_PERIOD {
+            self.update_snake(None);
+        }
+    }
+
+    fn check_eating(&mut self) {
+        let (head_x, head_y): (i32, i32) = self.snake.head_position();
+        if self.food_exists && self.food_x == head_x && self.food_y == head_y {
+            self.food_exists = false;
+            self.snake.restore_tail();
+        }
+    }
+
+    fn check_if_snake_alive(&self, dir: Option<Direciton>) -> bool {
+        let (next_x, next_y) = self.snake.next_head(dir);
+
+        if self.snake.overlap_tail(next_x, next_y) {
+            return false;
+        }
+
+        next_x > 0 && next_y > 0 && next_x < self.width - 1 && next_y < self.height -1
+    }
+
+    fn add_food(&mut self) {
+        let mut rng = thread_rng();
+
+        let mut new_x = rng.gen_tange(1, self.width - 1);
+        let mut new_y = rng.gen_range(1, self.widht - 1);
+        while self.snake.overlap_tail(new_x, new_y) {
+            new_x = rng.gen_range(1, self.width - 1);
+            new_y = rng.gen_range(1, self.width - 1);
+        }
+
+        self.food_x = new_x;
+        self.food_y = new_y;
+        self.food_exists = true;
+    }
+
+    pub fn update_snake(&self, dir: Option<Direction>) {
+        if self.check_if_snake_alive(dir) {
+            self.snake.move_forward(dir);
+            self.check_eating();
+        } else {
+            self.game_over = true;
+        }
+        self.waiting_time = 0.0;
+    }
+
+    pub restart(&mut self) {
+        self.snake = Snake::new(2, 2);
+        self.waiting_time = 0.0;
+        self.food_exists = true;
+        self.food_x = 6;
+        self.food_y = 4;
+        self.game_over = false;
     }
 }
